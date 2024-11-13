2d222048b0f9ac078306f2176bafe0f920926ada
Enock Mecheo <emm10042@nyu.edu>
2024-11-13 12:33:16 -0500

    Updated mysql port

--- config/db.mjs
+++ config/db.mjs
@@ -7,6 +7,7 @@ dotenv.config();
 // Create a new Sequelize instance using environment variables
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false // Disable logging; default: console.log
 });
